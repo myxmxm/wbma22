6dfdca2d3f90c2cf47135e60e801d815381f8164
myxmxm <myxmxm@gmail.com>
2022-01-12 13:55:57 +0200

    add ApiHooks.js, refactor, task http-c ready

--- components/List.js
+++ components/List.js
@@ -1,5 +1,6 @@
 import React, {useState, useEffect} from 'react';
 import {FlatList} from 'react-native';
+import {useMedia} from '../hooks/Apihooks';
 import {baseUrl} from '../utils/variables';
 import ListItem from './ListItem';
 
@@ -9,40 +10,9 @@ import ListItem from './ListItem';
 // const baseUrl = 'https://media.mw.metropolia.fi/wbma/';
 
 const List = () => {
-  const [mediaArray, setMediaArray] = useState([]);
-  // let mediaArray = [];
-  const loadMedia = async (start = 0, limit = 10) => {
-    try {
-      const response = await fetch(
-        `${baseUrl}media?start=${start}&limit=${limit}`
-      );
-      if (!response.ok) {
-        throw Error(response.statusText);
-      }
-      const json = await response.json();
-      const media = await Promise.all(
-        json.map(async (item) => {
-          const response = await fetch(baseUrl + 'media/' + item.file_id);
-          const mediaData = await response.json();
-          // console.log(mediaData);
-          return mediaData;
-        })
-      );
-      // console.log(json);
-      // console.log('test');
-      // mediaArray = json;
-      setMediaArray(media);
-    } catch (error) {
-      console.error(error);
-    }
-
-    console.log(mediaArray);
-  };
-  // call loadMedia() only once when the component is loaded
-  useEffect(() => {
-    loadMedia(0, 5);
-  }, []);
   // loadMedia();
+
+  const {mediaArray} = useMedia();
 
   return (
     <FlatList
