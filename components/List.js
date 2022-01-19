b41490412f33d575c9b4fab30f1e66b7a2117ad2
myxmxm <myxmxm@gmail.com>
2022-01-19 14:27:37 +0200

    update

--- components/List.js
+++ components/List.js
@@ -1,8 +1,9 @@
 import React from 'react';
 import {FlatList} from 'react-native';
-import {useMedia} from '../hooks/ApiHooks';
+
 import ListItem from './ListItem';
 import PropTypes from 'prop-types';
+import {useMedia} from '../hooks/Apihooks';
 
 const List = ({navigation}) => {
   const {mediaArray} = useMedia();
