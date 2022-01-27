ab2844e1ad432a274af252e6c3d5bc97aaac44ba
myxmxm <myxmxm@gmail.com>
2022-01-27 14:23:38 +0200

    Smarter forms validation task 2 ready

--- components/RegisterForm.js
+++ components/RegisterForm.js
@@ -1,16 +1,19 @@
 import React from 'react';
-import {View} from 'react-native';
+import {Alert, View} from 'react-native';
 import {useForm, Controller} from 'react-hook-form';
 import {useUser} from '../hooks/ApiHooks';
 import {Input, Button, Text} from 'react-native-elements';
+import {AlertCircle} from 'react-native-feather';
+import {PropTypes} from 'prop-types';
 
-const RegisterForm = () => {
+const RegisterForm = ({setFormToggle}) => {
   const {postUser, checkUsername} = useUser();
 
   const {
     control,
     handleSubmit,
     formState: {errors},
+    getValues,
   } = useForm({
     defaultValues: {
       username: '',
@@ -24,8 +27,13 @@ const RegisterForm = () => {
   const onSubmit = async (data) => {
     console.log(data);
     try {
+      delete data.confirmPassword;
       const userData = await postUser(data);
       console.log('register onSubmit', userData);
+      if (userData) {
+        Alert.alert('Success', 'User created successfully');
+        setFormToggle(true);
+      }
     } catch (error) {
       console.error(error);
     }
@@ -36,7 +44,11 @@ const RegisterForm = () => {
       <Controller
         control={control}
         rules={{
-          required: true,
+          required: {value: true, message: 'this is required!'},
+          minLength: {
+            value: 3,
+            message: 'Username has to be at least 3 characters',
+          },
           validate: async (value) => {
             try {
               const available = await checkUsername(value);
@@ -66,7 +78,15 @@ const RegisterForm = () => {
       <Controller
         control={control}
         rules={{
-          required: true,
+          required: {value: true, message: 'this is required!'},
+          minLength: {
+            value: 5,
+            message: 'Password has to be at least 5 characters',
+            pattern: {
+              // password: required, at least 5 characters Extra: at least one number and at least one capital letter of any language
+            },
+            // value: /(?=.*[\p{Lu}])(?=.*[0-9]).{8,}/u,
+          },
         }}
         render={({field: {onChange, onBlur, value}}) => (
           <Input
@@ -76,16 +96,49 @@ const RegisterForm = () => {
             autoCapitalize="none"
             secureTextEntry={true}
             placeholder="Password"
+            errorMessage={errors.password && errors.password.message}
           />
         )}
         name="password"
       />
-      {errors.password && <Text>This is required.</Text>}
 
       <Controller
         control={control}
         rules={{
-          required: true,
+          required: {value: true, message: 'this is required!'},
+          validate: async (value) => {
+            const {password} = getValues();
+            if (value === password) {
+              return true;
+            } else {
+              return 'Password not match';
+            }
+          },
+        }}
+        render={({field: {onChange, onBlur, value}}) => (
+          <Input
+            onBlur={onBlur}
+            onChangeText={onChange}
+            value={value}
+            autoCapitalize="none"
+            secureTextEntry={true}
+            placeholder="Confirm password"
+            errorMessage={
+              errors.confirmPassword && errors.confirmPassword.message
+            }
+          />
+        )}
+        name="confirmPassword"
+      />
+
+      <Controller
+        control={control}
+        rules={{
+          required: {value: true, message: 'this is required!'},
+          pattern: {
+            value: /\S+@\S+.\S+$/,
+            message: 'Not a valid email format',
+          },
         }}
         render={({field: {onChange, onBlur, value}}) => (
           <Input
@@ -94,14 +147,21 @@ const RegisterForm = () => {
             value={value}
             autoCapitalize="none"
             placeholder="Email"
+            errorMessage={errors.email && errors.email.message}
           />
         )}
         name="email"
       />
-      {errors.email && <Text>This is required.</Text>}
 
       <Controller
         control={control}
+        rules={{
+          required: {value: true, message: 'this is required!'},
+          minLength: {
+            value: 3,
+            message: 'Full name has to be at least 3 characters',
+          },
+        }}
         render={({field: {onChange, onBlur, value}}) => (
           <Input
             onBlur={onBlur}
@@ -109,6 +169,7 @@ const RegisterForm = () => {
             value={value}
             autoCapitalize="words"
             placeholder="Full name"
+            errorMessage={errors.full_name && errors.full_name.message}
           />
         )}
         name="full_name"
@@ -117,6 +178,10 @@ const RegisterForm = () => {
       <Button title="Submit" onPress={handleSubmit(onSubmit)} />
     </View>
   );
+};
+
+RegisterForm.propTypes = {
+  setFormToggle: PropTypes.func,
 };
 
 export default RegisterForm;
