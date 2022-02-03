24a56da387641b25498878697cdaec400f2dfd3f
myxmxm <myxmxm@gmail.com>
2022-02-03 10:58:06 +0200

    upload task a ready

--- utils/variables.js
+++ utils/variables.js
@@ -1,4 +1,5 @@
 const uploadsUrl = 'https://media.mw.metropolia.fi/wbma/uploads/';
 const baseUrl = 'https://media.mw.metropolia.fi/wbma/';
+const appId = 'myxmxmsadflajsd;fjljasd;fjk;jdfs;lkgh;j;lkjghdfjhlg';
 
-export {uploadsUrl, baseUrl};
+export {uploadsUrl, baseUrl, appId};
