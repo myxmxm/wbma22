474d0899a907c792e37aaaf3e780f0047e8e8444
myxmxm <myxmxm@gmail.com>
2022-02-10 15:27:41 +0200

    updata

--- views/MyFile.js
+++ views/MyFile.js
@@ -4,7 +4,7 @@ import PropTypes from 'prop-types';
 import List from '../components/List';
 
 const MyFiles = ({navigation}) => {
-  return <List navigation={navigation} myFilesOnly="true" />;
+  return <List navigation={navigation} myFilesOnly={true} />;
 };
 
 MyFiles.propTypes = {
